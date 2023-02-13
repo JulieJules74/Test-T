994ae11832b75e93da11ecfdddaa8955fbbf01d5
juliejules74 <124923671+JulieJules74@users.noreply.github.com>
2023-02-13 23:17:07 +0000

    added an exception

---
+++ main.py
@@ -0,0 +1,12 @@
+def main():
+  intro=("Hello Friends!\nWelcome to my weather app. ")
+  print(intro)
+main()
+
+try:
+  
+  city_zip = input("Please enter the zip code or city for where you want to see the weather: ")
+
+except:
+    zip = int()
+    city = input()
